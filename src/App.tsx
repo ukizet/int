fb73ed42f0d60e644fa6b57e04b4b5aa13626d4f
ukizet <ukikatuki@gmail.com>
2024-03-16 22:33:13 +0200

    init add

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
